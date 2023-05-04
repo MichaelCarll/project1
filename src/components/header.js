51edd3fa8ae3e7b0fbe5eb5b78a92f2d0b778d2a
Michael Carll <mikeycrl@gmail.com>
2023-05-04 08:32:27 -0500

    handling events

--- src/components/header.js
+++ src/components/header.js
@@ -13,11 +13,24 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 
+    hello = () => {
+        console.log('I was Clicked');
+    }
+
+    inputChange(event){
+        console.log(event.target.value);
+    }
+
     render(){
         return (
             <header>
-                <div className="logo">Logo</div>
-                <input/>
+                <div 
+                    className="logo"
+                    onClick={this.hello} //classes always need to use this when invoking references to functions
+                >Logo</div>
+                <input
+                    onChange={ (event)=> this.inputChange(event) }
+                />
             </header>
         )
     }
